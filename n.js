7415a2e8dfdb6a3d742236274395b86c9495c28d
nick-haiar-portfolio <nicholashaiar@gmail.com>
2017-02-20 21:26:33 -0500

    First commit, requirements at top.

---
+++ n.js
@@ -0,0 +1,6 @@
+//Greeter
+//When given a first name, last name, and optional language, it generates formal and informal greetings
+//Support English and Spanish
+//Reusable library/framework
+//Easy to type N$() structure.
+

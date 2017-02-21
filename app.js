169e44a3b2a24d1ec9d8d4cd64b4a47608ddd181
nick-haiar-portfolio <nicholashaiar@gmail.com>
2017-02-20 22:17:27 -0500

    Properties and Methods added.

---
+++ app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Nick on 2/20/2017.
+ */
+var n = N$('John', 'Doe');
+n.greet();
--- n.js
+++ n.js
@@ -3,4 +3,99 @@
 //Support English and Spanish
 //Reusable library/framework
 //Easy to type N$() structure.
+//jQuery
+//Window
 
+(function( global, $ ) {
+
+    var nJS = function(firstName, lastName, language) {
+        return new nJS.init( firstName, lastName, language );
+    };
+
+    var supportedLangs = ['en', 'es'];
+
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio de sesión'
+    };
+
+    nJS.prototype = {
+
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function() {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ' ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            // if undefined or null it will be coerced to 'false'
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting()
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            return this;
+        },
+
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' +
+                this.fullName());
+            }
+
+            return this;
+        },
+
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+
+    };
+
+    nJS.init = function(firstName, lastName, language) {
+
+        var self = this;
+        self.firstName = firstName || "";
+        self.lastName = lastName || "";
+        self.language = language || "en";
+    };
+
+    nJS.init.prototype = nJS.prototype;
+
+    global.nJS = global.N$ = nJS;
+
+}(window, jQuery));
